63c6522b4539f6cc1ed681f395c930230a3aca67
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-02-26 22:51:25 +0530

    commit add

---
+++ Lecture15_16/Session 15&16/project/app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+# Load model safely
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    return render_template(
+        "index.html", prediction_text="Species: {}".format(prediction[0])
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
