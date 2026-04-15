74b466123d8913816a37112e067146a022bf7f55
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-04-15 11:54:07 +0530

    commit add

--- Lecture30_34/Session 30-34/queans.py
+++ Lecture30_34/Session 30-34/queans.py
@@ -1,14 +1,17 @@
 # import os
 import google.generativeai as genai
+
 # from google import genai
 import streamlit as st
-st.title('Question-Answers')
+
+st.title("Question-Answers")
 
 client = genai.Client(api_key=st.secrets["GOOGLE_API_KEY"])
 
 user_input = st.text_area("Enter Your Question")
-if(st.button('Answer')):
+if st.button("Answer"):
     response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=user_input,)
+        model="gemini-2.5-flash",
+        contents=user_input,
+    )
     st.success(response.text)
